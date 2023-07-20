4f6bf498ca51a588da1328ae8265b4df558e2c74
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-07-20 22:10:53 +0300

    Add OkHttp to dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
     `java-library`
 }
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+}
+
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
