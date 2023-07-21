a56c85b8fd19b41cbf1459028c26426282202621
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-07-21 17:31:56 +0300

    Add JetBrains annotations to dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    compileOnly("org.jetbrains:annotations:24.0.1")
 }
 
 java {
