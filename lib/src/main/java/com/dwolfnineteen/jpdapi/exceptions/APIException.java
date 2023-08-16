bc14c9a16040f962d19b3f053543a4ebe1026246
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-08-16 22:14:26 +0300

    Make APIException abstract

--- lib/src/main/java/com/dwolfnineteen/jpdapi/exceptions/APIException.java
+++ lib/src/main/java/com/dwolfnineteen/jpdapi/exceptions/APIException.java
@@ -23,7 +23,7 @@ package com.dwolfnineteen.jpdapi.exceptions;
 
 import org.jetbrains.annotations.NotNull;
 
-public class APIException extends RuntimeException {
+public abstract class APIException extends RuntimeException {
     public APIException(@NotNull String message) {
         super(message);
     }
